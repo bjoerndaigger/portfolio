af7294eedc52f96db46dbabb094cc58bd5bad82e
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2023-10-20 16:51:05 +0200

    contact form

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
+  // scroll up animation
   isMouseOver = false;
+
+  // send mail
+  @ViewChild('myForm') myForm!: ElementRef;
+  @ViewChild('nameField') nameField!: ElementRef;
+  @ViewChild('emailField') emailField!: ElementRef;
+  @ViewChild('messageField') messageField!: ElementRef;
+  async sendMail() {
+    let nameField = this.nameField.nativeElement;
+    let emailField = this.emailField.nativeElement;
+    let messageField = this.messageField.nativeElement;
+
+    nameField.disabled = true;
+    emailField.disabled = true;
+    emailField.disabled = true;
+
+    let fd = new FormData();
+    fd.append('name', nameField.value);
+    fd.append('email', emailField.value);
+    fd.append('message', messageField.value);
+
+    await fetch('https://bjoerndaigger.de/send_mail/send_mail.php', {
+      method: 'POST',
+      body: fd,
+    });
+
+    nameField.disabled = false;
+    emailField.disabled = false;
+    emailField.disabled = false;
+  }
 }
