1a71f041d00fb6156e0ef35aa39a627c6f68dad0
Björn Daigger <124874016+bjoerndaigger@users.noreply.github.com>
2024-03-15 10:11:50 +0100

    changed domain

--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -12,7 +12,7 @@ export class PortfolioComponent {
       languages: 'JavaScript | HTML | CSS',
       description: 'Task manager inspired by the Kanban System: create, organize, and prioritize tasks with drag-and-drop functionality, while assigning users and categories.',
       img: 'assets/img/join_board.png',
-      url: 'https://join.bjoerndaigger.de/',
+      url: 'https://join.daigger.de/',
       github: 'https://github.com/bjoerndaigger/join'
     },
     {
@@ -20,7 +20,7 @@ export class PortfolioComponent {
       languages: 'JavaScript | HTML | CSS',
       description: 'A simple Jump-and-Run game based on an object-oriented approach. Help Pepe to find coins and salsa bottles to fight against a crazy chicken.',
       img: 'assets/img/el_pollo_loco.png',
-      url: 'https://el-pollo-loco.bjoerndaigger.de/',
+      url: 'https://el-pollo-loco.daigger.de/',
       github: 'https://github.com/bjoerndaigger/el-pollo-loco'
     },
     {
@@ -28,16 +28,8 @@ export class PortfolioComponent {
       languages: 'Angular | Typescript | Firebase',
       description: 'A very simple Customer Relationship Management system that works with CRUD functionality.',
       img: 'assets/img/simple_crm.png',
-      url: 'https://simple-crm.bjoerndaigger.de/',
+      url: 'https://simple-crm.daigger.de/',
       github: 'https://github.com/bjoerndaigger/simple-crm'
-    },
-    {
-      name: 'Portfolio Website',
-      languages: 'Angular | Typescript | SCSS',
-      description: 'My personal website, where I present various programming projects.',
-      img: 'assets/img/portfolio.png',
-      url: 'https://bjoerndaigger.de/',
-      github: 'https://github.com/bjoerndaigger/portfolio'
     },
   ];
 }
